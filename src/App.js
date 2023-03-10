7edc86ecbcc4583cd6218fe9df53e754856e8c66
seungbeen <seungbin803@gmail.com>
2023-03-10 12:17:11 +0900

    주석

--- src/App.js
+++ src/App.js
@@ -2,7 +2,25 @@ import './App.css';
 
 function App() {
   const name = '리액트';
-  return <div className='react'> {name} </div>
-}
+  return (
+    <>
+    { /* 주석은 이렇게 작성합니다. */ }
+      <div
+        className="react" // 시작 태그를 여러 줄로 작성하게 된다면 주석이 된다.
+        // 주석 되나요
+      > {name}
+      </div>
+      // 하지만 이런 주석이나
+      /* 이런 주석은 페이지에 그대로 나타나게 됩니다. */
+      <input/>
+      </>
+    );
+  }
+  <form>
+    성: <br/>
+    <input/><br/>
+    이름: <br/>
+    <input/>
+  </form>
 
 export default App;
