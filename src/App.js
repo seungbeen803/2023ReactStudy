61452d9f6b7196164a4bae0097d60af51445eed6
seungbeen <seungbin803@gmail.com>
2023-03-10 12:04:19 +0900

    React style 적용 방법3(가장많이쓰임)

--- src/App.js
+++ src/App.js
@@ -2,19 +2,7 @@ import './App.css';
 
 function App() {
   const name = '리액트';
-  return (
-  <div 
-    style={{ // 첫 번째 괄호는 js코드, 두번째 코드는 style 태그를 감싸주는 것
-      backgroundColor : 'yellow', // background-Color -> backgroundColor로 표현(카멜표기법)
-      color : 'aqua',
-      fontSize : '48px',
-      fontWeight : 'bold',
-      padding : 16 // 단위가 px
-    }}
-  >
-    name
-  </div>
- );
+  return <div className='react'> {name} </div>
 }
 
 export default App;
