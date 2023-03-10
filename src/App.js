9272cc9e4d8cc55800651e2aba0b8e422f7a1061
seungbeen <seungbin803@gmail.com>
2023-03-10 11:20:58 +0900

    React 여러 줄 작성 (조건문)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 
 function App() {
+  const name = 'React2';
   return (
     <div>
-      <h1> 리액트 안녕 </h1>
+      { name === '리액트' ? (
+        <h1> {name} 안녕 </h1>
+      ) : (
       <h2> 잘 작동하니? </h2>
+      )}
     </div>
   );
 }
