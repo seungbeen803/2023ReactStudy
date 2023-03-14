86b3b1a428fa221cefc8093e2c1e81d954b9e707
seungbeen <seungbin803@gmail.com>
2023-03-14 15:07:14 +0900

    props.을 적용하지 않아도 적용가능

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -2,13 +2,18 @@
 // function ----> = () => { }
 // function MyComponent { }
 const MyComponent = props => {
+  const {name, children } = props;
   return (
     <div> 
-      안녕하세요. 제 이름은 {props.name}입니다. <br />
-      children 값은 {props.children}
+      안녕하세요. 제 이름은 {name}입니다. <br />
+      children 값은 {children}
       입니다.
       </div>
   );
+};
+
+MyComponent.defaultProps =  {
+  name : "기본이름"
 };
 
 export default MyComponent
