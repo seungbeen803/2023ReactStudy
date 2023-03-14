18870690c135106a9bb421f0bdee511e68c04ef0
seungbeen <seungbin803@gmail.com>
2023-03-14 15:03:48 +0900

    하나의 자식 속성 값으로 들어가는 방법(children)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import MyComponent from "./MyComponent"; // 모듈 불러오기
 
 const  App = () =>  {
-    return <MyComponent name= "React" />
+    return <MyComponent>리액트</MyComponent> // 하나의 자식 속성 값으로 들어감
   };
 
 export default App;
--- src/MyComponent.js
+++ src/MyComponent.js
@@ -2,7 +2,17 @@
 // function ----> = () => { }
 // function MyComponent { }
 const MyComponent = props => {
-  return <div> 안녕하세요. 제 이름은 {props.name}입니다. </div>
-}
+  return (
+    <div> 
+      안녕하세요. 제 이름은 {props.name}입니다. <br />
+      children 값은 {props.children}
+      입니다.
+      </div>
+  );
+};
+
+MyComponent.defaultProps =  {
+  name : "기본이름"
+};
 
 export default MyComponent
