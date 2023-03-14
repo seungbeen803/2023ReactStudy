a1b82c2fdf5c83f5d585e861c9f35e3603f7eb77
seungbeen <seungbin803@gmail.com>
2023-03-14 14:40:32 +0900

    저번시간 복습

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  const name = '리액트';
-  return (
-    <>
-    { /* 주석은 이렇게 작성합니다. */ }
-      <div
-        className="react" // 시작 태그를 여러 줄로 작성하게 된다면 주석이 된다.
-        // 주석 되나요
-      > {name}
-      </div>
-      // 하지만 이런 주석이나
-      /* 이런 주석은 페이지에 그대로 나타나게 됩니다. */
-      <input/>
-      </>
-    );
+class App extends Component {
+  render() {
+    const name = "리액트";
+    return <div className="react"> {name} </div>
   }
-  <form>
-    성: <br/>
-    <input/><br/>
-    이름: <br/>
-    <input/>
-  </form>
+}
 
 export default App;
