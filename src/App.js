7ad872889a190937c67f9486940ba01fbe15e53d
seungbeen <seungbin803@gmail.com>
2023-03-17 11:34:51 +0900

    PropsTypes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import MyComponent from "./MyComponent"; // 모듈 불러오기
 
 const  App = () =>  {
-    return <MyComponent>React</MyComponent> // 하나의 자식 속성 값으로 들어감
+    return <MyComponent name="React" favoriteNumber={3}>리액트</MyComponent> // 하나의 자식 속성 값으로 들어감
   };
 
 export default App;
--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,12 +1,15 @@
 // MyComponent.js
 // function ----> = () => { }
 // function MyComponent { }
-const MyComponent = ({props}) => {
+import PropTypes from "prop-types"
+const MyComponent = ({name, favoriteNumber, children}) => {
   return (
     <div> 
-      안녕하세요. 제 이름은 {props.name}입니다. <br />
-      children 값은 {props.children}
+      안녕하세요. 제 이름은 {name}입니다. <br />
+      children 값은 {children}
       입니다.
+      <br/>
+      제가 좋아하는 숫자는 {favoriteNumber}입니다.
     </div>
   );
 };
@@ -14,5 +17,10 @@ const MyComponent = ({props}) => {
 MyComponent.defaultProps =  {
   name : "기본이름",
 };
+MyComponent.propTypes = {
+  name: PropTypes.string,
+  // 무조건 문자열 형태로 전달해야 한다.
+  favoriteNumber: PropTypes.number.isRequired,
+}
 
 export default MyComponent;
