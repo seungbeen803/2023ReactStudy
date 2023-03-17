e4a5f03ab0c709bb1e1bdb03ecbe31c278ef8c2b
seungbeen <seungbin803@gmail.com>
2023-03-17 12:20:39 +0900

    setState로 숫자를 1씩 증가시키는 작업을 console에 찍기

--- src/Counter.js
+++ src/Counter.js
@@ -21,20 +21,19 @@ class Counter extends Component {
         <button
         // onClick을 통해 버튼이 클릭되었을 때 호출할 함수를 지정
           onClick = { () => {
-            this.setState((prevState) => {
-              return {
-                number: prevState.number + 1
+            this.setState (
+              {
+                number: number + 1
+              },
+              () => {
+                console.log('방금 setState가 호출되었습니다.');
+                console.log(this.state);
               }
-            });
-            this.setState((prevState) => {
-              return {
-                number: prevState.number + 1
-              }
-            });
+            )
           }
         }
         >
-          +2
+          +1
         </button>
       </div>
     );
