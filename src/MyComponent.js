e8d4b982965ed90f8e2a4ead31094be1ed6f71a6
seungbeen <seungbin803@gmail.com>
2023-03-17 11:41:45 +0900

    클래스형 컴포넌트에서 props 사용하기

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,17 +1,24 @@
 // MyComponent.js
 // function ----> = () => { }
 // function MyComponent { }
+import { Component } from "react";
 import PropTypes from "prop-types"
-const MyComponent = ({name, favoriteNumber, children}) => {
-  return (
-    <div> 
-      안녕하세요. 제 이름은 {name}입니다. <br />
-      children 값은 {children}
-      입니다.
-      <br/>
-      제가 좋아하는 숫자는 {favoriteNumber}입니다.
-    </div>
-  );
+
+class MyComponent extends Component {
+  render() {
+    // this.props를 통해서 속성을 조회한다
+    // name, favoriteNumber, children에 값이 저장된다.
+    const { name, favoriteNumber, children } = this.props;
+    return (
+      <div> 
+        안녕하세요. 제 이름은 {name}입니다. <br />
+        children 값은 {children}
+        입니다.
+        <br/>
+        제가 좋아하는 숫자는 {favoriteNumber}입니다.
+      </div>
+    );
+  }
 };
 
 MyComponent.defaultProps =  {
