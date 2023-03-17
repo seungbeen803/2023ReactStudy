fc2e88c1bd5c1c5451cdc5c1671057a8874949d6
seungbeen <seungbin803@gmail.com>
2023-03-17 12:07:20 +0900

    state를 constructor에서 꺼내기

--- src/Counter.js
+++ src/Counter.js
@@ -1,21 +1,23 @@
 import { Component } from 'react';
 
 class Counter extends Component {
-  constructor(props) {
-    super(props); // 반드시 호출
-
-    // state의 초깃값 설정
-    this.state = { 
-      number : 0
+    state = { 
+      number : 0,
+      fixedNumber : 0
     };
-  }
+    
+  // 생성자 작성없이 state문을 작성하면 초깃값을 설정할 수 있도록
+  // 내부적으로 생성자가 실행되고 있음
   render() {
-    const {number} = this.state; // state를 조회할 때 this.state로 한다.
+    // this.state는 생성자에서 설정한 값을 조회해서
+    // number, fixedNumber 값을 저장
+    const {number, fixedNumber} = this.state;
     // this.state는 생성자에서 설정한 값을 가져와서
     // const {number}에서 number에 저장
     return (
       <div>
         <h1> { number } </h1>
+        <h2> 바뀌지 않는 값: {fixedNumber} </h2>
         <button
         // onClick을 통해 버튼이 클릭되었을 때 호출할 함수를 지정
           onClick = { () => {
@@ -23,8 +25,7 @@ class Counter extends Component {
             // setState메서드에 의해서만 state(number)
             // 값을 변경할 수 있다.
             this.setState({number: number+1});
-          }
-        }
+          }}
         >
           +1
         </button>
