96d7df9f9ddcfc404a94e3d0450514c95a11daed
seungbeen <seungbin803@gmail.com>
2023-03-28 14:51:48 +0900

    input 여러 개 다루기

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -3,18 +3,27 @@ import { Component } from "react";
 
 class EventPractice extends Component {
   state = {
+    usernaum: '',
     message: "",
   };
 
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    // 생성자 메서드는 this(현재 객체)와 메서드를 묶어주는 역할을 한다.
+  }
+
   handleChange = (e) => {
     this.setState({
-      message: e.target.value,
+      [e.target.name]: e.target.value,
     });
   };
 
   handleClick = () => {
-    alert(this.state.message);
+    alert(this.state.username + " : " + this.state.message);
     this.setState({
+      username: '',
       message: "",
     });
   };
@@ -25,14 +34,20 @@ class EventPractice extends Component {
         <h1>이벤트 연습</h1>
         <input
           type="text"
+          name="username"
+          placeholder="사용자 이름"
+          value={this.state.username}
+          onChange={this.handleChange}
+          />
+          <input
+          type="text"
           name="message"
           placeholder="아무거나 입력해보세요"
           value={this.state.message}
           onChange={this.handleChange}
           />
         <button
-          onClick={this.handleClick}>확인
-        </button>
+          onClick={this.handleClick}>확인</button>
       </div>
     );
   }
