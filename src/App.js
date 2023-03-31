5d8de2bc1517d6ea63d00e22fcac6ac70a6ba5ac
seungbeen <seungbin803@gmail.com>
2023-03-31 12:23:18 +0900

    ScrollBox

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 // 모듈 불러오기
 import { Component } from "react";
-import RefSample from "./RefSample";
-
+import ScrollBox from "./ScrollBox";
 class App extends Component {
     render() {
-        return <RefSample />
+        return (
+            <div>
+                <ScrollBox ref={(ref) => (this.ScrollBox = ref)} />
+                <button onClick={() => this.ScrollBox.scrollBottom()}>
+                    맨 밑으로
+                </button>
+            </div>
+        );
     }
 }
-
 export default App;
--- src/ScrollBox.js
+++ src/ScrollBox.js
@@ -2,6 +2,18 @@
 
 import { Component } from "react";
 class ScrollBox extends Component {
+  scrollBottom = () => {
+    const { scrollHeight, clientHeight } = this.box;
+
+    // 위 문장은 아래 코드와 같다.
+    // const scrollHeight = this.box.scrollHeight;
+    // const clientHeight = this.box.clientHeight;
+
+    this.box.scrollTop = scrollHeight - clientHeight;
+    // scrollHeight는 박스의 전체 높이
+    // clientHeight는 내부 박스를 픽셀로 반환한 것
+  };
+
   render() {
     const style = {
       border: "1px solid black",
@@ -12,8 +24,8 @@ class ScrollBox extends Component {
     };
 
     const innerStyle = {
-      width: "500%",
-      height: "150px",
+      width: "100%",
+      height: "650px",
       background: "linear-gradient(white, black)",
     };
 
