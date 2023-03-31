02d712b19fbe79945722ee6a668f354c17229ae0
seungbeen <seungbin803@gmail.com>
2023-03-31 12:08:33 +0900

    innerStyle width, height값 변경해보기

---
+++ src/ScrollBox.js
@@ -0,0 +1,32 @@
+// ScrollBox.js
+
+import { Component } from "react";
+class ScrollBox extends Component {
+  render() {
+    const style = {
+      border: "1px solid black",
+      height: "300px",
+      width: "300px",
+      overflow: "auto",
+      position: "relative"
+    };
+
+    const innerStyle = {
+      width: "500%",
+      height: "150px",
+      background: "linear-gradient(white, black)",
+    };
+
+    return (
+      <div
+        style={style}
+        ref={(ref) => {
+          this.box = ref;
+        }}
+      >
+        <div style={innerStyle} />
+      </div>
+    );
+  }
+}
+export default ScrollBox;
