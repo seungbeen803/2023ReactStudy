b230052392b6c57c65eca4dc6cec99fa590311dd
seungbeen <seungbin803@gmail.com>
2023-04-04 16:10:13 +0900

    데이터 추가 기능 구현하기

--- src/IterationSample1.js
+++ src/IterationSample1.js
@@ -10,23 +10,28 @@ const IterationSample1 = () => {
     ]);
 
   // nextId에 5를 저장. 새로운 항목을 추가하기 위해서 사용할 id
+  // [변수, 함수]
   const [inputText, setInputText] = useState(""); // eslint-disable-line no-unused-vars
   const [nextId, setNextId] = useState(5); // eslint-disable-line no-unused-vars
   // 새로운 항목을 추가할때 사용할 id
 
   const onChange = (e) => setInputText(e.target.value);
-  const onClick = () => {
-    const nextNames = names.concat({
-      id: nextId,
-      text: inputText,
-    });
-    // nextId에 1을 더해준다.
-    setNextId(nextId + 1);
-    // names 값을 업데이트한다.
-    setNextId(nextNames);
-    // inputText를 비운다.
-    setInputText("");
-  }
+  const onClick = // eslint-disable-line no-unused-vars
+    () => {
+      const nextNames = names.concat({
+        id: nextId,
+        text: inputText,
+      });
+      // nextId에 1을 더해준다.
+      setNextId(nextId + 1);
+      // names 값을 업데이트한다.
+      setNames(nextNames);
+      // inputText를 비운다.
+      setInputText("");
+    };
+
+
+
 
   const nameList = names.map((name) => <li key={name.id}>{name.text}</li>);
   return (
